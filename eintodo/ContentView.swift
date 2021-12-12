2b65121ae3b11f154cf4f9b2aa3e544fa07eba86
Duc Nhat Anh Nguyen <nducnhatanh@icloud.com>
2021-12-12 21:44:14 +0100

    CoreData-Entity definiert

--- eintodo/ContentView.swift
+++ eintodo/ContentView.swift
@@ -12,9 +12,9 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        sortDescriptors: [NSSortDescriptor(keyPath: \ToDo.timestamp, ascending: true)],
         animation: .default)
-    private var items: FetchedResults<Item>
+    private var items: FetchedResults<ToDo>
 
     var body: some View {
         NavigationView {
@@ -42,7 +42,7 @@ struct ContentView: View {
 
     private func addItem() {
         withAnimation {
-            let newItem = Item(context: viewContext)
+            let newItem = ToDo(context: viewContext)
             newItem.timestamp = Date()
 
             do {
