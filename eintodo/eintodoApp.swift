212eef75688bdb0eab65c3cc86ede77cda98a16d
Duc Nhat Anh Nguyen <nducnhatanh@icloud.com>
2021-12-12 21:17:11 +0100

    Initial Commit

---
+++ eintodo/eintodoApp.swift
@@ -0,0 +1,20 @@
+//
+//  eintodoApp.swift
+//  eintodo
+//
+//  Created by anh :) on 12.12.21.
+//
+
+import SwiftUI
+
+@main
+struct eintodoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
